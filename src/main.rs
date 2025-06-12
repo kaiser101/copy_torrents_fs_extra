a5bf611865353b1ab258f1f81f1afbd2c12c41f8
kaiser101 <kaiser101@gmail.com>
2025-06-13 00:54:56 +0530

    Use lms copy

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,28 @@
 #![allow(unused_variables)]
+use std::time::SystemTime;
+
 use fs_extra::dir::{move_dir_with_progress, CopyOptions, TransitProcess, TransitProcessResult};
 use fs_extra::error::Error;
 
+use lms::core::copy;
+use lms::parse::Flag;
+use std::io::Error as StdError;
+
 fn main() {
-    match copy_recursively_fs_extra() {
+    let now = SystemTime::now();
+
+    match move_dir_with_progress() {
         Ok(_) => println!("Files moved"),
         Err(e) => println!("Error {}", e),
+    }
+
+    match now.elapsed() {
+        Ok(elapsed) => {
+            println!("Files copied in {} seconds", elapsed.as_secs());
+        }
+        Err(e) => {
+            println!("Error: {e:?}");
+        }
     }
 }
 
@@ -13,7 +30,7 @@ pub fn copy_recursively_fs_extra() -> Result<(), Error> {
     let options = CopyOptions {
         overwrite: true,
         content_only: true,
-        buffer_size: 1024000,
+        buffer_size: 4096000,
         ..Default::default()
     };
 
@@ -24,6 +41,16 @@ pub fn copy_recursively_fs_extra() -> Result<(), Error> {
         "I:/Movies/New",
         &options,
         handle,
+    )?;
+
+    Ok(())
+}
+
+pub fn lms_copy() -> Result<(), StdError> {
+    let _ = copy(
+        "E:/Administrator/Downloads/Code samples/Node/torrent_client_js/torrents",
+        "I:/Movies/New",
+        Flag::from_bits_truncate(7),
     )?;
 
     Ok(())
